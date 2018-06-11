4d6d7961246b8f14602ed93a3450e0ae9d914a38
rizwanakhtarr <you@example.com>
2018-06-11 04:56:20 +0100

    added colors to cards using arrays in Java

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
 
-  
 
+function shuffle(arrayColor) {
+  var currentIndex = arrayColor.length,
+  temporaryValue, shuffleColorsRand;
+  while (0 != currentIndex) { //while there are colors to shuffle
 
+// one remaining
+  shuffleColorsRand = Math.floor(Math.random() * currentIndex);
+  currentIndex -= 1;
 
+//swap with current element
+  temporaryValue = arrayColor[currentIndex];
+  arrayColor[currentIndex] = arrayColor[shuffleColorsRand];
+  arrayColor[shuffleColorsRand] = temporaryValue;
+  }
+  return arrayColor;
+  }
 
-  });
+  var colors = ['red', 'blue', 'green', 'red', 'blue' , 'green' , 'black' , 'black' , 'yellow' ,'yellow', 'orange','orange','grey','grey', 'brown','brown' ];
+  shuffle(colors);
+  var i = 0;
+  $(".cards").each(function() {
+  $(this).css("background", colors[i++]);
+
+});
+
+});
